0846861abb6c9f4077257a8ac60344f408906ec3
Joseph Bettenhausen <21023218+jbettenh@users.noreply.github.com>
2022-12-04 11:09:42 +0100

    Add folium and map with overlays

--- groundhog/routes.py
+++ groundhog/routes.py
@@ -5,6 +5,7 @@ from flask import (
     request,
     session,
 )
+import folium
 from groundhog.helpers import login_required
 
 bp = Blueprint("routes", __name__, url_prefix="/")
@@ -38,7 +39,40 @@ def history():
 @bp.route("/map", methods=["GET"])
 @login_required
 def map_page():
-    return render_template("map.html")
+    start_coords = (42.375890, -71.114685)
+    folium_map = folium.Map(location=start_coords, zoom_start=17)
+
+    # Add layer for habitat
+    folium.raster_layers.WmsTileLayer(
+        url="https://www.sciencebase.gov:443/geoserver/CONUS_HabMap_2001/wms?SERVICE=WMS&",
+        name="Habitat",
+        fmt="image/png",
+        layers="Woodchuck (Marmota monax) mWOODx v1",
+        attribute="U.S. Geological Survey (USGS) - Gap Analysis Project (GAP), 2018, Woodchuck (Marmota monax) mWOODx_CONUS_2001v1 Habitat Map: U.S. Geological Survey data release, https://doi.org/10.5066/F7BG2MD4.",
+        transparent=True,
+        opacity=0.2,
+        overlay=True,
+        control=True,
+    ).add_to(folium_map)
+
+    # Add layer for range
+    folium.raster_layers.WmsTileLayer(
+        url="https://www.sciencebase.gov:443/geoserver/CONUS_Range_2001/wms?SERVICE=WMS&",
+        name="Range",
+        fmt="image/png",
+        layers="Woodchuck (Marmota monax) mWOODx v1",
+        attribute="U.S. Geological Survey (USGS) - Gap Analysis Project (GAP), 2018, Woodchuck (Marmota monax) mWOODx_CONUS_2001v1 Range Map: U.S. Geological Survey data release, https://doi.org/10.5066/F70864GK.",
+        transparent=True,
+        opacity=0.2,
+        overlay=True,
+        control=True,
+        show=False,
+    ).add_to(folium_map)
+
+    # Add a legend to hide/show layers
+    folium.LayerControl().add_to(folium_map)
+
+    return render_template("map.html", folium_map=folium_map._repr_html_())
 
 
 @bp.route("/zoo", methods=["GET"])
