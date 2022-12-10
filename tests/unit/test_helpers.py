23dbe9c0810b6aeadd47a1ac6e0fbd8401503cce
Joseph Bettenhausen <21023218+jbettenh@users.noreply.github.com>
2022-12-10 21:43:28 +0100

    Add helper tests

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -1,6 +1,25 @@
-from groundhog.helpers import error
+from groundhog.helpers import error, get_coordinates, get_geocode
 
 
 def test_error_handler():
     test_error = error("This is a test", 418)
     assert test_error.status_code == 302
+
+
+def test_get_coordinates():
+    loc = get_coordinates("45.130.83.145")
+
+    assert loc == (40.7425, -73.9877)
+
+
+def test_get_coordinates_none():
+    loc = get_coordinates("255.255")
+
+    assert loc == None
+
+
+def test_get_geocode():
+    address = "96 St, Manhattan, NY, 10020, USA"
+    geocode = get_geocode(address)
+
+    assert geocode == (40.7943793, -73.9719996)
